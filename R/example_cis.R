97296867ec5782c9fab2ed185034e37914344de2
Carlisle Rainey <carlislerainey@gmail.com>
2014-06-13 12:50:01 -0400

    add code to make example cis figure

---
+++ R/example_cis.R
@@ -0,0 +1,43 @@
+
+
+library(compactr)
+
+par(mfrow = c(1,1), mar = c(3,1,1,1), oma = c(0,0,0,0))
+eplot(xlim = c(-0.1, 1), ylim = c(.5, 5.5),
+      xlab = "Effect", anny = FALSE)
+abline(v = 0)
+
+ht <- 1
+est <- .05
+lwr <- -.01
+points(est, ht, pch = 19)
+lines(c(lwr, est + est - lwr ), c(ht,ht))
+text(est, ht, "(E)\nSmall, Precise, Not Significant", pos = 3)
+
+ht <- 2
+est <- .05
+lwr <- .01
+points(est, ht, pch = 19)
+lines(c(lwr, est + est - lwr ), c(ht,ht))
+text(est, ht, "(D)\nSmall, Precise, Significant", pos = 3)
+
+ht <- 3
+est <- .4
+lwr <- -.01
+points(est, ht, pch = 19)
+lines(c(lwr, est + est - lwr ), c(ht,ht))
+text(est, ht, "(C)\nLarge, Imprecise, Not Significant", pos = 3)
+
+ht <- 4
+est <- .4
+lwr <- .01
+points(est, ht, pch = 19)
+lines(c(lwr, est + est - lwr ), c(ht,ht))
+text(est, ht, "(B)\nLarge, Imprecise, Significant", pos = 3)
+
+ht <- 5
+est <- .4
+lwr <- .35
+points(est, ht, pch = 19)
+lines(c(lwr, est + est - lwr ), c(ht,ht))
+text(est, ht, "(A)\nLarge, Precise, Significant", pos = 3)
