2d72079b4e5579d14a7df9bdf5c332e416902e0a
Carlisle Rainey <carlislerainey@gmail.com>
2014-06-17 11:05:53 -0400

    update study labels in example cis figure

--- R/example_cis.R
+++ R/example_cis.R
@@ -20,7 +20,7 @@ est <- .05
 lwr <- -.01
 points(est, ht, pch = 19)
 lines(c(lwr, est + est - lwr ), c(ht,ht))
-text(est, ht, "(E)\nSmall, Precise, Not Significant", pos = 3)
+text(est, ht, "Study E\nSmall, Precise, Not Significant", pos = 3)
 
 # CI 2 
 ht <- 2
@@ -28,7 +28,7 @@ est <- .05
 lwr <- .01
 points(est, ht, pch = 19)
 lines(c(lwr, est + est - lwr ), c(ht,ht))
-text(est, ht, "(D)\nSmall, Precise, Significant", pos = 3)
+text(est, ht, "Study D\nSmall, Precise, Significant", pos = 3)
 
 # CI 3
 ht <- 3
@@ -36,7 +36,7 @@ est <- .4
 lwr <- -.01
 points(est, ht, pch = 19)
 lines(c(lwr, est + est - lwr ), c(ht,ht))
-text(est, ht, "(C)\nLarge, Imprecise, Not Significant", pos = 3)
+text(est, ht, "Study C\nLarge, Imprecise, Not Significant", pos = 3)
 
 # CI 4
 ht <- 4
@@ -44,7 +44,7 @@ est <- .4
 lwr <- .01
 points(est, ht, pch = 19)
 lines(c(lwr, est + est - lwr ), c(ht,ht))
-text(est, ht, "(B)\nLarge, Imprecise, Significant", pos = 3)
+text(est, ht, "Study B\nLarge, Imprecise, Significant", pos = 3)
 
 # CI 5
 ht <- 5
@@ -52,7 +52,7 @@ est <- .4
 lwr <- .35
 points(est, ht, pch = 19)
 lines(c(lwr, est + est - lwr ), c(ht,ht))
-text(est, ht, "(A)\nLarge, Precise, Significant", pos = 3)
+text(est, ht, "Study A\nLarge, Precise, Significant", pos = 3)
 
 # close graphics device
 dev.off()
