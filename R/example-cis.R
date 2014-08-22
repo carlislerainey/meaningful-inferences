6ad1662b161c3a3cecc1ec948cc07924e3b2879f
carlislerainey <carlislerainey@gmail.com>
2014-08-22 06:38:39 -0400

    rewrite the section on SS and MS

--- R/example-cis.R
+++ R/example-cis.R
@@ -1,5 +1,5 @@
 # Make sure that working directory is set properly
-# setwd("~/Dropbox/projects/meaningful-inferences")
+setwd("~/Dropbox/projects/meaningful-inferences")
 
 # clear workspace
 rm(list = ls())
@@ -8,11 +8,13 @@ rm(list = ls())
 library(compactr)
 
 # create figure
-pdf("doc/figs/example-cis.pdf", height = 6, width = 8)
+pdf("doc/figs/example-cis.pdf", height = 4, width = 5)
 par(mfrow = c(1,1), mar = c(3,1,1,1), oma = c(0,0,0,0))
-eplot(xlim = c(-0.1, 1), ylim = c(.5, 5.5),
+eplot(xlim = c(-0.3, 1), ylim = c(.5, 5.5),
       xlab = "Effect", anny = FALSE)
-#abline(v = 0)
+abline(v = 0, col = "grey50")
+abline(v = 0.25, lty = 2, col = "grey50")
+
 
 # CI 1
 ht <- 1
@@ -20,7 +22,7 @@ est <- .05
 lwr <- -.01
 points(est, ht, pch = 19)
 lines(c(lwr, est + est - lwr ), c(ht,ht))
-text(est, ht, "Study E\nSmall, Precise, Not Significant", pos = 3)
+text(est, ht, "Study E\nSmall, Precise, Not Significant", pos = 3, cex = .8)
 
 # CI 2 
 ht <- 2
@@ -28,7 +30,7 @@ est <- .05
 lwr <- .01
 points(est, ht, pch = 19)
 lines(c(lwr, est + est - lwr ), c(ht,ht))
-text(est, ht, "Study D\nSmall, Precise, Significant", pos = 3)
+text(est, ht, "Study D\nSmall, Precise, Significant", pos = 3, cex = .8)
 
 # CI 3
 ht <- 3
@@ -36,7 +38,7 @@ est <- .4
 lwr <- -.01
 points(est, ht, pch = 19)
 lines(c(lwr, est + est - lwr ), c(ht,ht))
-text(est, ht, "Study C\nLarge, Imprecise, Not Significant", pos = 3)
+text(est, ht, "Study C\nLarge, Imprecise, Not Significant", pos = 3, cex = .8)
 
 # CI 4
 ht <- 4
@@ -44,7 +46,7 @@ est <- .4
 lwr <- .01
 points(est, ht, pch = 19)
 lines(c(lwr, est + est - lwr ), c(ht,ht))
-text(est, ht, "Study B\nLarge, Imprecise, Significant", pos = 3)
+text(est, ht, "Study B\nLarge, Imprecise, Significant", pos = 3, cex = .8)
 
 # CI 5
 ht <- 5
@@ -52,7 +54,7 @@ est <- .4
 lwr <- .35
 points(est, ht, pch = 19)
 lines(c(lwr, est + est - lwr ), c(ht,ht))
-text(est, ht, "Study A\nLarge, Precise, Significant", pos = 3)
+text(est, ht, "Study A\nLarge, Precise, Significant", pos = 3, cex = .8)
 
 # close graphics device
 dev.off()
